6053ecfb040b37a6884c20d589c93c311d7f7605
David Madner <david.madner@posteo.at>
2016-02-13 17:46:24 +0100

    feat(reader): Add new option showRectangles to display live scanning results

--- src/directives/reader.js
+++ src/directives/reader.js
@@ -1,5 +1,8 @@
 import quagga from 'quagga/dist/quagga';
 
+require('./reader.css');
+const tmpl = require('./reader.html');
+
 export default () => {
 
     return {
@@ -7,9 +10,11 @@ export default () => {
         scope: {
             type: '@',
             onDetected: '&',
-            onProcessed: '&'
+            onProcessed: '&',
+            showRectangles: '@'
+
         },
-        template: '<div id="interactive" class="viewport"></div>',      // Fallback until https://github.com/serratus/quaggaJS/pull/89 has been merged
+        templateUrl: tmpl,
         bindToController: true,
         controllerAs: 'vm',
         controller: ReaderCtrl
@@ -43,6 +48,31 @@ function ReaderCtrl($scope) {
     quagga.onDetected(_onDetected);
 
     function _onProcessed(result) {
+
+        if(vm.showRectangles) {
+            var drawingCtx = quagga.canvas.ctx.overlay,
+                drawingCanvas = quagga.canvas.dom.overlay;
+
+            if (result) {
+                if (result.boxes) {
+                    drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                    result.boxes.filter(function (box) {
+                        return box !== result.box;
+                    }).forEach(function (box) {
+                        quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+                    });
+                }
+
+                if (result.box) {
+                    quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+                }
+
+                if (result.codeResult && result.codeResult.code) {
+                    quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+                }
+            }
+        }
+
         if(vm.onProcessed) {
             vm.onProcessed({result: result});
         }
